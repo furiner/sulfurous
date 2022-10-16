583f915484bed00364b926e9b531c1786dc9fc49
irisu01 <nekomarisakirisame@gmail.com>
2022-10-16 15:07:23 -0400

    add utilities

--- lib/util/Util.ts
+++ lib/util/Util.ts
@@ -1,5 +1,16 @@
-class Util {
+import { RouteFunction } from "../structures/types/RouteFunction";
+import { WebsocketFunction } from "../structures/types/WebsocketFunction";
+
+export class Util {
     constructor() {
         throw new Error("This class cannot be instantiated.");
+    }
+
+    static isRouteFunction(func: Function): func is RouteFunction {
+        return "isRouteFunction" in func;
+    }
+
+    static isWebsocketFunction(func: Function): func is WebsocketFunction {
+        return "isWebsocketFunction" in func;
     }
 }
