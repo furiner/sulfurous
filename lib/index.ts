51d7e81cac79832a7387e4a5fa60b924027771d4
irisu01 <nekomarisakirisame@gmail.com>
2022-10-17 11:25:03 -0400

    chore(framework): add certain exports for test cases

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,17 @@
+/* APPLICATION */
 export * from "./server/Application";
+export * from "./server/Server";
+
+/* INTERFACES */
+export * from "./interfaces/ApplicationOptions";
+export * from "./interfaces/RouteDescriptor";
+export * from "./interfaces/CookieData";
+
+/* DEFAULT STRUCTURES */
+export * from "./controllers/DefaultController";
+
+/* STRUCTURES */
+export * from "./structures/Controller";
 
 /* DECORATORS */
 export * from "./structures/decorators/Get";
