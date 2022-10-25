d1485cb60c49d928a15334f7334a1abca2c7ea54
SMJSGaming <sjoerd.t@live.nl>
2022-10-25 21:20:45 +0200

    fix(core): Added abstraction to exclusively super classes

--- lib/structures/Integration.ts
+++ lib/structures/Integration.ts
@@ -1,16 +1,17 @@
 import { EventEmitter } from "events";
 import { DefaultIntegrationOptions, IntegrationOptions } from "../interfaces/IntegrationOptions";
-import { Application } from "../server/Application";
 
-export class Integration extends EventEmitter {
+export abstract class Integration extends EventEmitter {
     /**
      * The main ID of the integration
      */
     public id: string;
+    public options: IntegrationOptions;
 
     constructor(id: string, options: IntegrationOptions = DefaultIntegrationOptions) {
         super();
 
         this.id = id;
+        this.options = options;
     }
 }
