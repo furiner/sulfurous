118efe55944bff3b9fcdd5753990d38711818af2
irisu01 <nekomarisakirisame@gmail.com>
2022-10-25 14:37:34 -0400

    chore(sulfurous): add integration exports to index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -12,6 +12,9 @@ export * from "./controllers/DefaultController";
 
 /* STRUCTURES */
 export * from "./structures/Controller";
+export * from "./structures/Integration";
+export * from "./structures/requests/Request";
 
 /* DECORATORS */
 export * from "./structures/decorators/Get";
+export * from "./structures/decorators/IntegrationEvent";
